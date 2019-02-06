ccc7ab9df85664b2fd895f4a9a506c938fa2a9ec
sersht <sergey.shemchuk.t@gmail.com>
2019-02-06 22:58:41 +0200

    Fix sparse_matrix, update readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# university-projects
+# University projects  
+
+A lot of need to be finished (see comments in projects themselves).
+From time to time improving and adding code.
--- sparse_matrix.py
+++ sparse_matrix.py
@@ -1,3 +1,6 @@
+# Need: errors handling, refactor, increase scalability of D/S,
+# check in insertion if element exists, add tests
+
 class SparseMatrixNode:
     def __init__(self, position, value, right=None, bottom=None):
         self.position = position
@@ -7,16 +10,32 @@ class SparseMatrixNode:
 
 
 class SparseMatrix:
-    def __init__(self, rows_count=0, columns_count=0, elements_list=[]):
-        self.columns_count, self.rows_count = columns_count, rows_count
-        self.columns, self.rows = [None] * columns_count, [None] * rows_count
+    def __init__(self, rows_count=0, columns_count=0, elements=[], matrix=[]):
+        self.columns_count = columns_count
+        self.rows_count = rows_count
+        self.columns = [None] * self.columns_count
+        self.rows = [None] * self.rows_count
+        
+        if (len(matrix) > 0):
+            self.rows_count = len(matrix)
+            self.columns_count = len(matrix[0]) if self.rows_count > 0 else 0
 
-        for x in elements_list:
-            node = SparseMatrixNode(x[0], x[1])
-            self.__insert(node)
+            for i in range(len(matrix)):
+                for j in range(len(matrix[i])):
+                    if matrix[i][j] != 0:
+                        node = SparseMatrixNode((i, j), matrix[i][j])
+                        self.__insert(node)
+        
+        if (len(elements) > 0):
+            for x in elements:
+                node = SparseMatrixNode(x[0], x[1])
+                self.__insert(node)
 
+        # Assume that columns_count * rows_count fits in memory
     def __repr__(self):
-        view_list = [["0"] * self.columns_count for i in range(self.rows_count)]
+        view_list = [
+            ["0"] * self.columns_count for i in range(self.rows_count)]
+
         for i in range(self.rows_count):
             node = self.rows[i]
             while node is not None:
@@ -48,7 +67,7 @@ class SparseMatrix:
             return
 
         while curr_node.right is not None:
-            if node.position[0] < curr_node.right:
+            if node.position[0] < curr_node.right.position[0]:
                 node.right = curr_node.right
                 curr_node.right = node
                 return
@@ -70,7 +89,7 @@ class SparseMatrix:
             return
 
         while curr_node.bottom is not None:
-            if node.position[0] < curr_node.bottom:
+            if node.position[1] < curr_node.bottom.position[1]:
                 node.bottom = curr_node.bottom
                 curr_node.bottom = node
                 return
@@ -78,6 +97,22 @@ class SparseMatrix:
 
         curr_node.bottom = node
         node.bottom = None
+
+    def __find_element(self, position):
+        for i in range(self.rows_count):
+            node = self.rows[i]
+            while node is not None:
+                if node.position == position:
+                    return node
+                node = node.right
+        return None
+
+    def change_element(self, position, value):
+        search_result = self.__find_element(position)
+        if search_result is None:
+            self.__insert(SparseMatrixNode(position, value))
+        else:
+            search_result.value = value
 
     def get_non_zeros(self):
         non_zeros = list()
@@ -88,10 +123,10 @@ class SparseMatrix:
                 node = node.right
         return non_zeros
 
-    # def __init__(self, matrix):
-    #    pass
 
-
-elems = [((0, 0), -2), ((3, 3), 11), ((2, 3), 23)]
-mt = SparseMatrix(10, 10, elems)
-print(mt.get_non_zeros())
+elems = [((0, 0), -2), ((3, 3), 11), ((2, 3), 23),
+         ((4, 3), 22), ((1, 1), 24), ((0, 1), -12)]
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+mt = SparseMatrix(rows_count=5, columns_count=5, elements=elems)
+mt.change_element((4, 2), 101)
+print(mt)
