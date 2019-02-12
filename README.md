d038a72a04a603f3e1900437d2f16bd0ec437580
sersht <sergey.shemchuk.t@gmail.com>
2019-02-12 14:18:45 +0200

    Update README. Fix bugs, add get_first() in sparse_matrix. Add lotka_volterra project

--- README.md
+++ README.md
@@ -5,3 +5,32 @@ A lot of need to be finished (see comments in projects themselves).
 From time to time improving and adding code.  
 
 ## Sparse Matrix
+
+The example of memory efficient implementation of Data Structure to store and traverse matrices with a lot of zero-values.  
+
+### Needs to be done  
+
+* Input validating  
+* Errors handling  
+* Increase scalability (for matrix representation)  
+* Additional functionality:  
+    1. Insertion checking  
+    2. Cycle-traversing for each row and column  
+* Tests  
+
+### Class interface  
+
+* __from_elements(elements, rows_count, columns_count)__  
+create matrix of size rows_count * columns_count from non-zero-values elements list  
+
+* __from_matrix(matrix)__  
+create from existing matrix  
+
+* __change_element(position, value)__  
+change value at position to value  
+
+* __get_non_zeros()__  
+get list of non-zero elements  
+
+* __get_first(row_idx, col_idx)__  
+get first node with non-zero value in row or column  
--- sparse_matrix.py
+++ sparse_matrix.py
@@ -1,7 +1,3 @@
-# Need: errors handling, refactor, increase scalability of D/S (for greater dimensions),
-# check in insertion if element exists, add tests
-
-
 class SparseMatrixNode:
     def __init__(self, position, value, right=None, bottom=None):
         self.position = position
@@ -9,6 +5,14 @@ class SparseMatrixNode:
         self.right = right
         self.bottom = bottom
 
+    def __repr__(self):
+        return str(self.value)
+
+    def get_right(self):
+        return self.right
+
+    def get_bottom(self):
+        return self.bottom
 
 class SparseMatrix:
     def __init__(self, rows_count=0, columns_count=0):
@@ -17,21 +21,19 @@ class SparseMatrix:
         self.columns = [None] * self.columns_count
         self.rows = [None] * self.rows_count
 
-
     @staticmethod
     def from_matrix(matrix=[]):
         rows_count = len(matrix)
         columns_count = len(matrix[0]) if rows_count > 0 else 0
-        
+
         created = SparseMatrix(rows_count, columns_count)
-        
+
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if matrix[i][j] != 0:
                     created._insert(SparseMatrixNode((i, j), matrix[i][j]))
-        
-        return created
 
+        return created
 
     @staticmethod
     def from_elements(elements=[], rows_count=0, columns_count=0):
@@ -40,10 +42,10 @@ class SparseMatrix:
             created._insert(SparseMatrixNode(x[0], x[1]))
         return created
 
-
     # Assume that columns_count * rows_count fits in memory
     def __repr__(self):
-        view_list = [["0"] * self.columns_count for i in range(self.rows_count)]
+        view_list = [
+            ["0"] * self.columns_count for i in range(self.rows_count)]
 
         for i in range(self.rows_count):
             node = self.rows[i]
@@ -59,76 +61,73 @@ class SparseMatrix:
 
         return view
 
-
     def _insert(self, node):
-        self._insert_node(node)
+        self._insert_node(node, in_row=True)
         self._insert_node(node, in_row=False)
 
-
-    def _insert_node(self, node, in_row=True):
+    def _insert_node(self, node, in_row):
         COORDINATE = 0 if in_row else 1
-        
-        if in_row:
-            curr_node = self.rows[node.position[COORDINATE]]
-        else:
-            curr_node = self.columns[node.position[COORDINATE]]
+
+        def get_node(matrix, position, in_row):
+            if in_row:
+                return matrix.rows[position]
+            else:
+                return matrix.columns[position]
+
+        def get_next(node, in_row):
+            if in_row:
+                return node.right
+            else:
+                return node.bottom
+
+        def set_node(matrix, position, in_row, value):
+            if in_row:
+                matrix.rows[position] = value
+            else:
+                matrix.columns[position] = value
+
+        def set_next(node, in_row, value):
+            if in_row:
+                node.right = value
+            else:
+                node.bottom = value
+
+        curr_node = get_node(self, node.position[COORDINATE], in_row)
 
         if curr_node is None:
-            if in_row:
-                self.rows[node.position[COORDINATE]] = node
-            else:
-                self.columns[node.position[COORDINATE]] = node
-            return
-            
-        if node.position[COORDINATE] < curr_node.position[COORDINATE]:
-            if in_row:
-                self.rows[node.position[COORDINATE]] = node
-                node.right = curr_node
-            else:
-                self.columns[node.position[COORDINATE]] = node
-                node.bottom = curr_node
+            set_node(self, node.position[COORDINATE], in_row, node)
             return
 
-        if in_row:
-            while curr_node.right is not None:
-                if node.position[COORDINATE] < curr_node.right.position[COORDINATE]:
-                    node.right = curr_node.right
-                    curr_node.right = node
-                    return
-                curr_node = curr_node.right
-        else:
-            while curr_node.bottom is not None:
-                if node.position[COORDINATE] < curr_node.bottom.position[COORDINATE]:
-                    node.bottom = curr_node.bottom
-                    curr_node.bottom = node
-                    return
-                curr_node = curr_node.bottom
+        if node.position[COORDINATE ^ 1] < curr_node.position[COORDINATE ^ 1]:
+            set_node(self, node.position[COORDINATE], in_row, node)
+            set_next(node, in_row, curr_node)
+            return
 
-        if in_row:
-            curr_node.right = node
-            node.right = None
-        else:
-            curr_node.bottom = node
-            node.bottom = None    
+        while get_next(curr_node, in_row) is not None:
+            if node.position[COORDINATE ^ 1] < get_next(curr_node, in_row).position[COORDINATE ^ 1]:
+                set_next(node, in_row, get_next(curr_node, in_row))
+                set_next(curr_node, in_row, node)
+                return
+            curr_node = get_next(curr_node, in_row)
 
+        set_next(curr_node, in_row, node)
+        set_next(node, in_row, None)
 
     def _find_element(self, position):
         for i in range(self.rows_count):
-            node = self.rows[i] 
+            node = self.rows[i]
             while node is not None:
                 if node.position == position:
                     return node
                 node = node.right
         return None
 
-
     def change_element(self, position, value):
         search_result = self._find_element(position)
-        if search_result is None: 
+        if search_result is None:
             self._insert(SparseMatrixNode(position, value))
-        else: 
+        else:
             search_result.value = value
-
 
     def get_non_zeros(self):
         non_zeros = list()
@@ -138,14 +137,19 @@ class SparseMatrix:
                 non_zeros.append((node.position, node.value))
                 node = node.right
         return non_zeros
-
     
+    def get_first(self, row_idx=-1, col_idx=-1):
+        if row_idx != -1:
+            return self.rows[row_idx]
+        else:
+            return self.columns[col_idx]
+
 elems = [((0, 0), -2), ((3, 3), 11), ((2, 3), 23),
-         ((4, 3), 22), ((1, 1), 24), ((0, 1), -12)]
-matrix = [[0, 0, 0, 0], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+         ((4, 3), 22), ((1, 1), 24), ((0, 1), -12), ((9, 0), 666), ((0, 9), 1388)]
+# matrix = [[0, 0, 0, 0], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
 
 mt = SparseMatrix.from_elements(elems, 10, 10)
-# mt.change_element((1,1), 101)
+# mt.change_element((1, 1), 101)
 
 print(mt)
-# print(mt.get_non_zeros())
+print(mt.get_non_zeros())
